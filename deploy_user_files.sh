c95760d244767232d89771a9a44447c772bb17b8
Anurag Roy <anuragr9847@gmail.com>
2022-06-28 18:50:38 +0530

    Refactor script for ease of maintainance and future expansion
    - Define helper functions to receiving user input, confirming
      input(Y/N), running external command with dry run functionality,
      displaying info and error, exiting on fatal error, and verifying if
      the script is run on either dry run or no-confirm mode.
    - Use a separate sed script and copy_file function to configure and
      deploy pacman config file.
    - Use `eval` throughout for cases where dry running is required, so that
      commands to run can be easily set in strings and printed or run
    - Remove unnecessary unset commands
    - Use `mktemp` instead of a hardcoded temp file
    - Update comments
    - Arrange code blocks so that all functions are at the top
    - Make all important variables UPPERCASE
    - The location of file list, the sed script for configuring pacman,
      the username, enabling dry run and no confirm are configurable through
      environment variables
    - Check for owner only after finding an existing directory

--- deploy_user_files.sh
+++ deploy_user_files.sh
@@ -2,7 +2,7 @@
 
 # user_files - Anurag Roy's dotfiles and config files alongwith scripts
 # to copy and deploy them.
-#    Copyright (C) 2021 Anurag Roy
+#    Copyright (C) 2021, 2022 Anurag Roy
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -17,117 +17,167 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-### Copies a list of files, from `deploy_files.csv`, to their appropriate
-### locations.
-###
-### See `deploy_files.csv` to see what files are being copied.
-###
-### Requires root permissions to function properly.
+## Copies a list of files, defaultly from `deploy_files.csv`, to their
+## appropriate locations.
+##
+## See `deploy_files.csv` to see what files are being copied.
+##
+## Requires root permissions to function properly.
 
-### Copies a file to a specified directory.
-###
-### [$1] is the file name in the repo and [$2] the full path of the new file
-### (location tree will be newly made if non-existent).
-###
-### The files to be copied are present in the `./files/` directory.
-###
-### If the specified file already exists, then `cp` prompts for user input
-### regarding whether the file should be overidden or not. If allowed to do so,
-### creates a backup of the file with ".old" suffix.
+## Verifies if the script is run in "dry-run" mode.
+##
+## Successful only if the environment variable "DRY_RUN" is set to "1".
+is_dry_run() {
+  [ "$DRY_RUN" -eq 1 ] && return 0
+  return 1
+}
+
+## Verifies if the script is run in "no-confirm" mode.
+##
+## Successful only if the environment variable "NO_CONFIRM" is set to "1".
+is_no_confirm() {
+  [ "$NO_CONFIRM" -eq 1 ] && return 0
+  return 1
+}
+
+## Helper for displaying informative messages.
+##
+## Takes a string [$1] as the message to display. This is typically used to
+## inform the successful execution of an external command.
+##
+## An empty message results in a no-op.
+display_info() {
+  [ -n "$1" ] && printf "[INFO] %s\n" "$1"
+}
+
+## Helper for displaying error messages.
+##
+## Takes a string [$1] as the message to display. An empty message results in a
+## no-op.
+display_error() {
+  [ -n "$1" ] && printf "[ERROR] %s\n" "$1"
+}
+
+## Helper for exiting script in case of a fatal error.
+##
+## Takes a string [$1] as the message to display. An empty message results in a
+## no-op.
+die() {
+  display_error "$1" && exit 1
+}
+
+## Helper for receiving user input.
+##
+## Takes an optional prompt string [$1].
+user_input() {
+  # Print the prompt to tty before taking input
+  [ -n "$1" ] && printf "%s: " "$1" > /dev/tty
+  read -r input < /dev/tty
+  echo "$input"
+}
+
+## Helper for taking user input for yes/no confirmation.
+##
+## Returns a non-failing exit status for "yes" or "y" in any case.
+confirm() {
+  # If NO_CONFIRM is set, return as if the user has already confirmed the
+  # operation.
+  is_no_confirm && return 0
+
+  input="$(user_input "$1 (y/Y/Yes/YES to confirm)")"
+  case "$input" in
+    [yY]|[yY][eE][sS]) return 0 ;;
+    *) return 1
+  esac
+}
+
+## Returns the root of the path in [$1] from which it differs with path [$2].
+##
+## Preferably, [$1] should be longer than [$2].
+##
+## ```sh
+## $ x="/home/rarg"; y="/home/rarg/some_dir"
+## $ echo "${x#$y/}" ('/' is added to remove the leading character)
+## /home/rarg (no match for "/home/rarg/some_dir" in "/home/rarg")
+## $ echo "${y#$x/}"
+## some_dir
+## ```
+non_matching_path_root() {
+  shorter_path="$2"
+  longer_path="$1"
+  echo "${longer_path#"$shorter_path/"}" | cut -d '/' -f1
+}
+
+## Helper for running external commands with "dry running".
+##
+## Takes a command string [$1] to run and an optional message [$2] to display
+## at successful execution.
+run_command() {
+  # If the script is dry run, print the command and exit, else use `eval` to run
+  # the command.
+  is_dry_run && { printf "[DRY RUN] %s\n" "$1"; return 0; }
+  eval "$1" && display_info "$2"
+}
+
+## Wrapper for `chown` to recursively change file [$2] ownership to a specified
+## user [$1].
+change_owner() {
+  run_command "chown -R $1: $2"
+}
+
+## Copies a file to a specified directory.
+##
+## [$1] is the file name to be copied and [$2] is the full path of the new
+## file(location tree will be newly made if non-existent).
+##
+## If the specified file already exists, then prompts for user input to confirm
+## the overwrite. If allowed to do so, creates a backup of the file with ".old"
+## suffix.
 copy_file() {
-  echo
-  ### If the file already exists, provide a diff
+  # If the file already exists, provide a diff
   if [ -e "$2" ]; then
-    echo
     diff -usr --color "$2" "$1"
     echo
-    if [ -z "$NO_CONFIRM" ]; then
-      printf "Overwrite %s? (y/Y for yes)\t" "$2"
-      read -r overwrite < /dev/tty
-      echo "$overwrite" | grep -q 'y\|Y' || return
-    fi
-    if [ -n "$DRY_RUN" ]; then
-      printf "[DRY RUN] cp -prT %s %s.old\n" "$2" "$2"
-    else
-      ### Create a backup of the old file before copying
-      cp -prT "$2" "$2.old" && echo "[INFO] Old $2 copied to $2.old."
-    fi
+    confirm "Overwrite \"$2\"?" || return 1
+    # Create a backup of the old file before copying
+    run_command "cp -prT $2 $2.old" "Old $2 copied to $2.old"
   fi
-  if [ -n "$DRY_RUN" ]; then
-    printf "[DRY RUN] mkdir -pv %s\n" "$(dirname "$2")"
-    printf "[DRY RUN] cp -vrT %s %s\n" "$1" "$2"
-    return
-  fi
-  mkdir -pv "$(dirname "$2")"
-  cp -vrT "$1" "$2"
+  run_command "mkdir -pv $(dirname "$2")"
+  run_command "cp -vrT $1 $2"
+}
+
+## Configures pacman.
+##
+## More details of configuration are available at the [PACMAN_CONFIG] file.
+config_pacman_conf() {
+  # Check if the sed script exists; return if missing
+  [ -f "./$PACMAN_CONFIG" ] || { \
+    display_error "Cannot find \"$PACMAN_CONFIG\" to configure pacman."; return 1; }
+  conf="/etc/pacman.conf"
+  display_info "Configuring pacman"
+  # Make required changes to a temporary file
+  new_conf="$(mktemp)"
+  sed -Ef "$PACMAN_CONFIG" "$conf" > "$new_conf"
+  # Use [copy_file] to display changes and ask for confirmation as necessary
+  copy_file "$new_conf" "$conf"
   echo
 }
 
-### Wrapper for chown for changing file [$2] ownership to a specified user [$1].
-change_owner() {
-  [ -n "$DRY_RUN" ] && printf "[DRY RUN] chown -R %s: %s\n" "$1" "$2" && return
-  chown -R "$1": "$2"
-}
-
-### Between two paths of equal or unequal length, removes the longest matching
-### common path and provides the root folder of the resulting location.
-###
-### ```sh
-### $ x="/home/anurag"; y="/home/anurag/some_folder"
-### $ echo "${x#$y/}" ('/' is added to remove the leading character)
-### /home/anurag (no match for "/home/anurag/some_folder" in "/home/anurag")
-### $ echo "${y#$x/}"
-### some_folder
-### ```
-###
-### The function additionally cuts and returns the first field which would be
-### the root of the non-matching location.
-non_matching_path_root() {
-  full_path="$2"
-  remove_path="$1"
-  echo "${full_path#$remove_path/}" | cut -d '/' -f1
-}
-
-### Adds some configuration options to pacman.
-###
-### This function edits the config file of pacman to enable color, tabular list
-### of transactions(upgrade, sync and remove) and an easter egg.
-config_pacman_conf() {
-  config_string='s/^#Color$/Color/; s/^#VerbosePkgLists$/VerbosePkgLists/; /^#\?ILoveCandy$/d; /^#ParallelDownloads.*$/a ILoveCandy'
-  printf "[INFO] Configuring pacman\n"
-  [ -n "$DRY_RUN" ] && \
-    printf "[DRY RUN] sed --in-place='.old' '%s' /etc/pacman.conf\n" "$config_string" && \
-    sed "$config_string" /etc/pacman.conf && return
-  sed --in-place='.old' "$config_string" /etc/pacman.conf
-  printf "[INFO] Old /etc/pacman.conf copied to /etc/pacman.conf.old.\n"
-}
-
-### In case the user uses the `.profile` and `.rootprofile` file as-is, reminds
-### them to create the bash history folder.
-make_bash_history() {
+## Displays the LICENSE applied to this project.
+##
+## GNU General Public License - Version 3.0
+show_license() {
   cat << END
 
-In case you haven't changed the "HISTFILE" value in ".profile" and ".rootprofile"
-and copied those files to your system, run:
-
-	mkdir -pv ~/.local/share/bash
-	sudo mkdir -pv /root/.local/share/bash
-
-for bash to create the history file at the new location.
+Deploy user files Copyright (C) 2021, 2022 Anurag Roy
+    This program comes with ABSOLUTELY NO WARRANTY. This is free software,
+    and you are welcome to redistribute it under certain conditions.
+    For more details see http://www.gnu.org/licenses/gpl-3.0.html.
 
 END
 }
 
-### Parse options
-for i in "$@"; do
-  case "$i" in
-    "-n") DRY_RUN=1 ;;
-    "-y") NO_CONFIRM=1 ;;
-    *) ;;
-  esac
-done
-
-### Displays this message if dry run is activated.
+## Displays this message if dry run is activated.
 dry_run_message() {
   cat << END
 The script has been asked to run in "DRY RUN" mode.
@@ -138,7 +188,7 @@ only display the commands it will execute upon normal mode.
 END
 }
 
-### Displays this message if no confirm is activated.
+## Displays this message if no confirm is activated.
 no_confirm_message() {
   cat << END
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!CAUTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -156,111 +206,138 @@ YOU HAVE BEEN WARNED.
 END
 }
 
-### Displays the LICENSE applied to this project.
-###
-### GNU General Public License - Version 3.0
-show_license() {
+## Displays finishing instructions after file deployment.
+finish_instructions() {
   cat << END
+Done deploying files. Finishing instructions follow:
 
-Deploy user files Copyright (C) 2021 Anurag Roy
-    This program comes with ABSOLUTELY NO WARRANTY. This is free software,
-    and you are welcome to redistribute it under certain conditions.
-    For more details see http://www.gnu.org/licenses/gpl-3.0.html.
+- The Firefox user settings are at "$home". Move this file to the desired
+  profile directory to apply the settings.
+
+- In case you haven't changed the "HISTFILE" value in ".profile" and
+  ".rootprofile" and copied those files to your system, run:
+
+    mkdir -pv ~/.local/share/bash
+    sudo mkdir -pv /root/.local/share/bash
+
+  for bash to create the history file at the new location.
+
+- Make sure to log out and login again for the deployed files to take effect.
 
 END
 }
 
-### Show LICENSE information
+## Important environment variables
+
+## The location where the files to be deployed are listed.
+##
+## The file must follow formatting/listing conventions detailed in the README.
+##
+## By default, "deploy_files.csv" is used, which should be located at the
+## current directory.
+FILE_LIST="${FILE_LIST:-"deploy_files.csv"}"
+
+## Sed script configuring pacman.
+##
+## An empty value skips pacman configuration.
+##
+## By default, "pacman.sed" is used, which should be located at the current
+## directory.
+PACMAN_CONFIG="${PACMAN_CONFIG:-"pacman.sed"}"
+
+## Denotes if the script is run in dry run mode.
+##
+## If enabled, the script will only show the deploying commands without actually
+## executing the commands.
+##
+## Disabled by default.
+DRY_RUN="${DRY_RUN:-"0"}"
+
+## Denotes if the script is run in no-confirm mode.
+##
+## If enabled, the script will not ask for confirmation for overwriting existing
+## files.
+##
+## Disabled by default.
+NO_CONFIRM="${NO_CONFIRM:-"0"}"
+
+# Show LICENSE information
 show_license
 
-### Check root permissions
-[ "$(id -u)" -ne 0 ] && \
-  { echo "[ERROR] Copying user files needs root privileges to function properly." && exit 1; }
+# Check root permissions
+[ "$(id -u)" -ne 0 ] && die "Copying user files needs root privileges to function properly."
 
-### Check if 'deploy_files.csv' exists and is readable
-[ -r ./deploy_files.csv ] || \
-  { echo "[ERROR] Could not find \"deploy_files.csv\" in the current folder. Are you sure it exists?" && exit 1; }
+# Check if [FILE_LIST] exists and is readable
+[ -r "./$FILE_LIST" ] || die "Cannot find \"$FILE_LIST\" to get the files to be deployed."
 
-### If no confirm is activated, displays a message
-[ -n "$NO_CONFIRM" ] && no_confirm_message
+# If no confirm is activated, displays a message
+is_no_confirm && no_confirm_message
 
-### If dry run is activated, displays a message
-[ -n "$DRY_RUN" ] && dry_run_message
+# If dry run is activated, displays a message
+is_dry_run && dry_run_message
 
-### Get user name through direct shell input
-echo "Enter the username. <Return> to use current username. <Control-C> to abort.";
-read -r USERNAME;
+# Get the user name if it is not provided through the environment.
+USERNAME="${USERNAME:-$(user_input "Enter the username. <Return> to use current username. <Control-C> to abort.")}"
 
-### [USERNAME] should be [SUDO_USER] by default.
+# [USERNAME] should be [SUDO_USER] by default.
 USERNAME=${USERNAME:-"$SUDO_USER"}
 
-### The location of the user home directory.
-###
-### This is used to substitute the "~" character in the locations specified in
-### "deploy_files.csv"
-USER_HOME="/home/$USERNAME"
+## The location of the user home directory.
+##
+## This is used to substitute the "~" character in the locations specified in
+## [FILE_LIST] file.
+home="/home/$USERNAME"
 
-### Check if USER_HOME actually exists
-[ -d "$USER_HOME" ] || \
-  { echo "[ERROR] \"$USER_HOME\" doesn't exist! Ensure that the username is correct." && exit 1; }
+# Check if [home] actually exists
+[ -d "$home" ] || die "\"$home\" doesn't exist! Ensure that the username is correct."
 
-### Configure pacman if it exists(for Arch-based distros)
-command -v pacman && config_pacman_conf
+# Configure pacman if it exists(for Arch-based distros)
+command -v pacman > /dev/null 2>&1 && config_pacman_conf
 
-### Construct a temporary file to read by removing comments from
-### "deploy_files.csv".
-sed '/^#\|^$/d' deploy_files.csv > /tmp/files.csv
+# Construct a temporary file to read by removing comments from [FILE_LIST].
+temp="$(mktemp)"
+sed '/^#\|^$/d' "$FILE_LIST" > "$temp"
 
-### Set field separator to read file.
+# Set field separator to read file.
 IFS=","
 
-### The actual loop to read and deploy files.
-###
-### [file] is the filename in the repo, [newfile] the file name to be copied as
-### and [locations] the list of locations to be copied to.
+# The actual loop to read and deploy files.
+# [file] is the filename in the repo, [newfile] the file name to be copied as
+# and [locations] the list of locations to be copied to.
 while read -r file newfile locations; do
   for location in $locations; do
-    ### Skip empty locations, for the sake of better csv formatting
+    # Skip empty locations, for the sake of better csv formatting
     echo "$location" | grep -q '^$' && continue
-    ### Replace "~" by [USER_HOME].
-    ###
-    ### Since the script is run with elevated privileges, "~" will refer to
-    ### "/root" instead of the user's home directory.
-    location=$(echo "$location" | sed "s|^\~|$USER_HOME|")
-    ### Only absolute file paths are allowed.
-    ###
-    ### The script skips the location if it fails this condition.
+    # Replace "~" by [home], otherwise "~" will refer to "/root" instead
+    # of the user's home directory since the script is run with elevated
+    # privileges.
+    location=$(echo "$location" | sed "s|^\~|$home|")
+    # Only absolute file paths are allowed. Skip locations otherwise.
     echo "$location" | grep -q '^/.*' || \
-      { echo "[ERROR] \"$location\" is not an absolute path. Skipping this location." && continue; }
-    ### "/." is added to prevent `dirname` to strip path without checking owner
-    ### in the first iteration.
-    dir="$location/."
+      { display_error "\"$location\" is not an absolute path. Skipping this location." && continue; }
+    dir="$location"
     new_location="$location/$newfile"
-    ### Traverse upward through the given location until an owner is found.
-    while [ -z "$owner" ]; do
+    # Traverse upward through the given location until a directory exists
+    until [ -d "$dir" ]; do
       dir="$(dirname "$dir")"
-      owner=$([ -d "$dir" ] && stat -c '%U' "$dir")
-      case "$owner" in
-        "") ;;
-        "root") copy_file "./files/$file" "$new_location" ;;
-        ### If the location is owned by anyone other than the root user, use
-        ### `chown -R` to change owner of the newly created files/folders to
-        ### that user after they have been copied/created.
-        *) copy_file "./files/$file" "$new_location" && \
-              change_owner "$owner" "$dir/$(non_matching_path_root "$dir" "$new_location")" ;;
-      esac
     done
-    ### Reset [owner] for the next location.
-    owner=''
+    owner=$(stat -c '%U' "$dir")
+    case "$owner" in
+      "") ;;
+      "root") copy_file "./files/$file" "$new_location" ;;
+      # If the location is owned by anyone other than the root user, use
+      # `chown -R` to change owner of the newly created files/directories to
+      # that user after they have been copied/created.
+      *) copy_file "./files/$file" "$new_location" && \
+        change_owner "$owner" "$dir/$(non_matching_path_root "$dir" "$new_location")" ;;
+    esac
+    echo
+    # Reset [owner] for the next location.
+    unset owner
   done
-done < /tmp/files.csv
+done < "$temp"
 
-if [ -z "$DRY_RUN" ]; then
-  echo "[INFO] Done deploying files."
-  echo "Make sure to log out and login again for the deployed files to take effect."
-  make_bash_history
-fi
+# If the script has not been dry run, display finishing instructions.
+is_dry_run || finish_instructions
 
-unset full_path remove_path config_string location new_location dir owner USERNAME USER_HOME NO_CONFIRM DRY_RUN IFS
-unset -f copy_files non_matching_path_root config_pacman_conf show_license make_bash_history change_owner dry_run_message no_confirm_message
 exit 0
---
+++ pacman.sed
@@ -0,0 +1,24 @@
+## Sed script to configure pacman.
+##
+## Enables:
+## * Color
+## * Tabular list of transactions(upgrade, sync, remove)
+## * Easter egg
+## * Anurag Roy's unofficial arch repository
+
+# Uncomment option to enable color
+s/^#Color$/Color/
+
+# Uncomment option to enable tabular list of transactions
+s/^#VerbosePkgLists$/VerbosePkgLists/
+
+# Delete any commented option for easter egg; add it after the last listed
+# option(which should be ParallelDownloads)
+/^#?ILoveCandy$/d
+/^#ParallelDownloads.*$/a ILoveCandy
+
+# Append details of the unofficial repository at the end of the file
+$a \
+\
+[royarg-repo] \
+Server = https://royarg02.github.io/$repo/os/$arch
